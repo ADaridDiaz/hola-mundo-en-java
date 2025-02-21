33307b95a054fb05ed8b45e0f8f49e98c79f75e2
Angel Diaz Sanchez <angelux504x@gmail.com>
2025-02-21 00:37:25 -0600

    fixing index 9

--- public/index.php
+++ public/index.php
@@ -3,36 +3,16 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-/**
- * Registrar el cargador automático generado por Composer
- * Esto permite cargar todas las dependencias del proyecto.
- */
+define('LARAVEL_START', microtime(true));
+
 require __DIR__.'/../vendor/autoload.php';
 
-/**
- * Encender la aplicación
- * Aquí se crea una nueva instancia de la aplicación Laravel.
- */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-/**
- * Crear una instancia del kernel HTTP
- * El kernel maneja las solicitudes y respuestas.
- */
 $kernel = $app->make(Kernel::class);
 
-/**
- * Capturar la solicitud HTTP entrante
- */
-$request = Request::capture();
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-/**
- * Manejar la solicitud y enviar la respuesta al navegador
- */
-$response = $kernel->handle($request)->send();
-
-/**
- * Terminar la ejecución de la aplicación
- * Aquí se ejecutan tareas posteriores, como cerrar conexiones.
- */
 $kernel->terminate($request, $response);
