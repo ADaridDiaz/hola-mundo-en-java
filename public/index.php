0e549455fb5f2000bba33d92ddf87fc49a5163d6
Angel Diaz Sanchez <angelux504x@gmail.com>
2025-02-21 00:15:41 -0600

    fixing index 7

--- public/index.php
+++ public/index.php
@@ -4,17 +4,20 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 /**
- * Cargar el autoload de Composer
+ * Registrar el cargador automático generado por Composer
+ * Esto permite cargar todas las dependencias del proyecto.
  */
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /**
- * Cargar la aplicación Laravel
+ * Encender la aplicación
+ * Aquí se crea una nueva instancia de la aplicación Laravel.
  */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 /**
  * Crear una instancia del kernel HTTP
+ * El kernel maneja las solicitudes y respuestas.
  */
 $kernel = $app->make(Kernel::class);
 
@@ -24,16 +27,12 @@ $kernel = $app->make(Kernel::class);
 $request = Request::capture();
 
 /**
- * Manejar la solicitud y generar la respuesta
+ * Manejar la solicitud y enviar la respuesta al navegador
  */
-$response = $kernel->handle($request);
+$response = $kernel->handle($request)->send();
 
 /**
- * Enviar la respuesta al navegador
- */
-$response->send();
-
-/**
- * Finalizar la ejecución de la aplicación
+ * Terminar la ejecución de la aplicación
+ * Aquí se ejecutan tareas posteriores, como cerrar conexiones.
  */
 $kernel->terminate($request, $response);
