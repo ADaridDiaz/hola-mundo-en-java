4ff5ec76969255bedd11df6ba59ac690d53dac03
Angel Diaz Sanchez <angelux504x@gmail.com>
2025-02-21 06:37:01 -0600

    prueba tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,13 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        'node_modules/preline/dist/*.js',
-
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./node_modules/preline/dist/*.js"
     ],
-
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+      extend: {},
     },
-
-    plugins: [require('preline/plugin'),forms],
-};
+    plugins: [],
+  };
