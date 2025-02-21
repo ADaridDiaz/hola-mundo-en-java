0ff79d1282d117e386b0669db128e681870b8c8e
Angel Diaz Sanchez <angelux504x@gmail.com>
2025-02-20 23:10:36 -0600

    fixing index y revert app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -17,9 +16,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-// Manejar la solicitud y enviar la respuesta
-$response = $app->handleRequest(Request::capture());
-$response->send();
-
-$app->terminate();
--- public/index.php
+++ public/index.php
@@ -4,14 +4,18 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
+// Cargar el autoloader de Composer
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Cargar la aplicación de Laravel
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Crear una instancia del kernel HTTP
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+// Manejar la solicitud y enviar la respuesta
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
