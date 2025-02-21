2438a3e38cde67d7c11a915fb5290b179f9bab0b
Angel Diaz Sanchez <angelux504x@gmail.com>
2025-02-20 23:43:48 -0600

    fixing index 5

--- public/index.php
+++ public/index.php
@@ -5,13 +5,11 @@ use Illuminate\Http\Request;
 
 /**
  * Cargar el autoload de Composer
- * Esto permite que Laravel cargue todas sus dependencias.
  */
 require __DIR__.'/../vendor/autoload.php';
 
 /**
  * Cargar la aplicación Laravel
- * Aquí se crea una nueva instancia de la aplicación.
  */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
@@ -28,10 +26,10 @@ $request = Request::capture();
 /**
  * Manejar la solicitud y enviar la respuesta al navegador
  */
-$response = $kernel->handle($request)->send();
+$response = $kernel->handle($request);
+$response->send();
 
 /**
  * Finalizar la ejecución de la aplicación
- * Aquí se ejecutan tareas posteriores, como liberar recursos.
  */
 $kernel->terminate($request, $response);
